a6ff03005e992742117754c48239dd1635f5ca51
jfxio <34043999+jfxio@users.noreply.github.com>
2023-06-12 10:59:46 -0700

    Simple node script and module using ESM modules

---
+++ src/main.ts
@@ -0,0 +1,4 @@
+import { reverse, greet } from './utl/index.js'
+
+console.log(`Greet: ${greet('Joe')}`)
+console.log(`Reverse: ${reverse('one two three')}`)
---
+++ src/utl/index.ts
@@ -0,0 +1,7 @@
+export function reverse(s: string): string {
+    return s.split(' ').reverse().join('_')
+}
+
+export function greet(n: string): string {
+    return n ? `Hello ${n}` : `Yo!`
+}
