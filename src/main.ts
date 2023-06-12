038c4d24ca8434eec04fc360798a55255f918d9c
jfxio <34043999+jfxio@users.noreply.github.com>
2023-06-12 11:36:18 -0700

    Use external module

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
+import chalk from 'chalk'
+
 import { reverse, greet } from './utl/index.js'
 
 console.log(`Greet: ${greet('Joe')}`)
 console.log(`Reverse: ${reverse('one two three')}`)
+console.log(chalk.blue(`Water is your friend.`))
