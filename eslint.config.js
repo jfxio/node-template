ff1664734544ebb20938dc8f098781a251a20aa3
J Frisbie <jf@bigdynamo.org>
2026-07-14 11:26:55 -0700

    baseline eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,63 +1,71 @@
-// Not @ts-check because we can't type eslint-plugin-jasmine
-// at the momeent. We add it to "ingnores" here and removed it
-// from "includes" in tsconfig.json. Also note the projectService
-// true parser option means tsconfig.json controls what files
-// processed (or at least get parsed by TS).
+// @ts-check
+
+// // Not @ts-check because we can't type eslint-plugin-jasmine
+// // at the momeent. We add it to "ingnores" here and removed it
+// // from "includes" in tsconfig.json. Also note the projectService
+// // true parser option means tsconfig.json controls what files
+// // processed (or at least get parsed by TS).
 
 import esLint from '@eslint/js'
-import prettierLint from 'eslint-config-prettier'
-import jasmine from 'eslint-plugin-jasmine'
-import tsDoc from 'eslint-plugin-tsdoc'
+import {defineConfig} from 'eslint/config'
+// import prettierLint from 'eslint-config-prettier'
+// import jasmine from 'eslint-plugin-jasmine'
+// import tsDoc from 'eslint-plugin-tsdoc'
 import tsLint from 'typescript-eslint'
 
-export default tsLint.config(
-  {
-    ignores: ['bin/**', 'eslint.config.js'],
-  },
-  {
-    languageOptions: {
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-    plugins: {
-      jasmine,
-      tsDoc,
-    },
-  },
-  esLint.configs.recommended,
-  jasmine.configs.recommended,
-  tsLint.configs.strictTypeChecked,
-  tsLint.configs.stylisticTypeChecked,
-  {
-    rules: {
-      // Rely on typescript to flag unused variables
-      '@typescript-eslint/no-unused-vars': 'off',
-      // Revert to recommended from strict
-      '@typescript-eslint/restrict-template-expressions': [
-        'error',
-        {
-          allow: [{name: ['Error', 'URL', 'URLSearchParams'], from: 'lib'}],
-          allowBoolean: true,
-          allowNullish: true,
-          allowNumber: true,
-          allowRegExp: true,
-        },
-      ],
-      // Allow infinte loops
-      '@typescript-eslint/no-unnecessary-condition': [
-        'error',
-        {
-          allowConstantLoopConditions: true,
-        },
-      ],
-      // Only warn about focus tests (should get removed before merge)
-      'jasmine/no-focused-tests': 'warn',
-      'tsDoc/syntax': 'warn',
-    },
-  },
+// export default tsLint.config(
+export default defineConfig({
+  files: ['**/*.{js,ts}'],
+  ignores: ['bin/**'],
+  extends: [esLint.configs.recommended, tsLint.configs.recommended],
+})
+//   {
+//     ignores: ['bin/**', 'eslint.config.js'],
+//   },
+//   {
+//     languageOptions: {
+//       parserOptions: {
+//         projectService: true,
+//         tsconf igRootDir: import.meta.dirname,
+//       },
+//     },
+//     plugins: {
+//       jasmine,
+//       tsDoc,
+//     },
+//   },
+//   esLint.configs.recommended,
+//   jasmine.configs.recommended,
+//   tsLint.configs.strictTypeChecked,
+//   tsLint.configs.stylisticTypeChecked,
+//   {
+//     rules: {
+//       // Rely on typescript to flag unused variables
+//       '@typescript-eslint/no-unused-vars': 'off',
+//       // Revert to recommended from strict
+//       '@typescript-eslint/restrict-template-expressions': [
+//         'error',
+//         {
+//           allow: [{name: ['Error', 'URL', 'URLSearchParams'], from: 'lib'}],
+//           allowBoolean: true,
+//           allowNullish: true,
+//           allowNumber: true,
+//           allowRegExp: true,
+//         },
+//       ],
+//       // Allow infinte loops
+//       '@typescript-eslint/no-unnecessary-condition': [
+//         'error',
+//         {
+//           allowConstantLoopConditions: true,
+//         },
+//       ],
+//       // Only warn about focus tests (should get removed before merge)
+//       'jasmine/no-focused-tests': 'warn',
+//       'tsDoc/syntax': 'warn',
+//     },
+//   },
 
-  // Last to turn off any formatting lints
-  prettierLint,
-)
+//   // Last to turn off any formatting lints
+//   prettierLint,
+// )
